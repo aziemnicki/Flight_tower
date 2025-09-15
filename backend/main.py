07c0ebb367578d7d13c455068e7efc5b2ed4f68b
aziemnicki <a-ziemnicki@wp.pl>
2025-09-16 01:09:13 +0200

    Removed api folder

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routes import health, flights, geo
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health.router, prefix="/health", tags=["health"])
+app.include_router(flights.router, prefix="/flights", tags=["flights"])
+app.include_router(geo.router, prefix="/geo", tags=["geo"])
