6026263d2c8d9f91aacf5e74e28c653acc95d6e6
aziemnicki <a-ziemnicki@wp.pl>
2025-09-16 00:28:10 +0200

    Fixed paths

--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
 # frontend-app/api/index.py
+import os 
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
