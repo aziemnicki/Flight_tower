34b821b7bd2b620dcc147e120651b96ff08ade5b
aziemnicki <a-ziemnicki@wp.pl>
2025-09-19 22:00:59 +0200

    Working fix of dynamic backend

--- flight-tower/next.config.mjs
+++ flight-tower/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
-      },
-    ]
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
